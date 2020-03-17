0503f27257e58892e15f20b9a2b6192bd68457fd
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-17 18:00:18 -0500

    edit

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import logo from 'react'
 import './App.css';
-import Header from './components/header'
+import Header from './components/Header'
+import Footer from './components/Footer';
 
 
 
--- react-portfolio/src/components/Header.js
+++ react-portfolio/src/components/Header.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import './style.css'
 export default class Header extends Component {
   render() {
-    let resumeData = this.props.resumeData;
     return (
       <React.Fragment>
           <header id="home">
-
+          
           </header>
       </React.Fragment>
       );
