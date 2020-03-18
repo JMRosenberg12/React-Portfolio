57237e22c9db7a3c2d195e5971f9112878798bce
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-18 08:27:11 -0500

    edit

--- react-portfolio/src/components/aboutme.js
+++ react-portfolio/src/components/aboutme.js
@@ -5,7 +5,6 @@ class About extends Component {
   render() {
     return(
       <div>
-        <h1>About Page</h1>
         <div class="container">
         <div class="row">
           <div class="col">
