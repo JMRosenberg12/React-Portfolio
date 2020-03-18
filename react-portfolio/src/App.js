47ac82481e2838169b7e09e94213240b38432183
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-18 10:08:09 -0500

    edit

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,6 +3,9 @@ import logo from 'react'
 import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer';
+import About from './components/aboutme';
+import Contact from './components/contact';
+import Portfolio from './componentsPortfolio'
 import {
   BrowserRouter as Router,
   Route,
@@ -55,25 +58,6 @@ class App extends Component {
   }
 }
   
- 
-    function About() {
-      return <h2>About Me</h2>;
-    }
-    function Contact() {
-      return <h2>Contact</h2>
-    }
-    function Portfolio() {
-      return (
-        <div>
-          <h2>Portfolio</h2>
-          <ul>
-        <li>
-        </li>
-      </ul>
-      </div>
-      
-      )
-      }
     
 
  
--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -1,7 +1,7 @@
 import React , {Component} from 'react';
 import Contact from 'react';
 
- export class Contact extends Component{
+export class Contact extends Component{
     render() {
       return(
         <div class="container">
