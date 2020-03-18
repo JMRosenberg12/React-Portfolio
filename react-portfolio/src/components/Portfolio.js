9bcd34b8cf618b8ba7cb4f898227c9427108b703
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-18 10:24:02 -0500

    edit

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -33,12 +33,12 @@ export default class Porfolio extends Component {
                     <a className="btn btn-success" href="https://github.com/JMRosenberg12/burger" role="button">Github Repository to Burger App</a>
                 </div>
                 <div className="col-sm">
-                  <img src="./assets/images/Screen Shot 2020-02-17 at 8.13.13 AM.png"> </img>
+                  <img src="./assets/images/Screen Shot 2020-02-17 at 8.13.13 AM.png" alt=""> </img>
                   <a className="btn btn-primary" href="https://serene-dawn-05814.herokuapp.com/" role="button">Note Taker</a>
                   <a className="btn btn-success" href="https://github.com/JMRosenberg12/Note-Taker" role="button">Github Repoistory to Note Taker</a>
                 </div>
                 <div className="col-sm">
-                  <img src="./assets/images/Screen Shot 2020-02-17 at 8.04.13 AM.png"> </img>
+                  <img src="./assets/images/Screen Shot 2020-02-17 at 8.04.13 AM.png" alt=""> </img>
                   <a className="btn btn-primary" href="https://spiderfly.herokuapp.com" role="button">Project 2</a>
                   <a className="btn btn-success" href="https://github.com/icek567/Project-2" role="button">Github Repository to Project 2</a>
                 </div>
