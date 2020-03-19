7c438d86bd9ab0092277cc20e0dc8d90983fad6f
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-19 08:18:40 -0500

    edit

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import WDScheduler from './images'
+
 export default class Porfolio extends Component {
   render() {
    
@@ -11,35 +11,35 @@ export default class Porfolio extends Component {
               </header>
               <div className="row">
                 <div className="col-sm">
-                    <img src={WDScheduler.png} alt="Work Day Scheduler"> </img>
+                    <img src="./images/WDScheduler.png" alt="Work Day Scheduler"> </img>
                     <a className="btn btn-primary" href="https://jmrosenberg12.github.io/Day-Planner/" role="button">Day Planner</a>
                     <a className="btn btn-success" href="https://github.com/JMRosenberg12/Day-Planner" role="button">Github Repository to DP</a>
         
                 </div>
                 <div className="col-sm">
-                    <img src="./assets/images/PasswordGenerator.png" alt="Password Generator"> </img>
+                    <img src="./images/PasswordGenerator.png" alt="Password Generator"> </img>
                     <a className="btn btn-primary" href="https://jmrosenberg12.github.io/Password-Generator/" role="button">Password Generator</a>
                     <a className="btn btn-success" href="https://github.com/JMRosenberg12/Password-Generator" role="button">Github Repository to PG</a>
                 </div>
                 <div className="col-sm">
-                    <img src="./assets/images/Project1.png" alt="Project 1"> </img>
+                    <img src="./images/Project1.png" alt="Project 1"> </img>
                     <a className="btn btn-primary" href="https://andrelbarreto.github.io/ProjectOne/index.html" role="button">Project 1</a>
                     <a className="btn btn-success" href="https://github.com/JMRosenberg12/ProjectOne" role="button">Github Repository to ProjectOne</a>
                 </div>
                 </div>
                 <div className="row">
                   <div className="col-sm">
-                    <img src="./assets/images/Burger.png" alt="Burger"> </img>
+                    <img src="./images/Burger.png" alt="Burger"> </img>
                     <a className="btn btn-primary" href="https://polar-fortress-86358.herokuapp.com" role="button">Burger App</a>
                     <a className="btn btn-success" href="https://github.com/JMRosenberg12/burger" role="button">Github Repository to Burger App</a>
                 </div>
                 <div className="col-sm">
-                  <img src="./assets/images/NoteTaker.png" alt="Note Taker"> </img>
+                  <img src="./images/NoteTaker.png" alt="Note Taker"> </img>
                   <a className="btn btn-primary" href="https://serene-dawn-05814.herokuapp.com/" role="button">Note Taker</a>
                   <a className="btn btn-success" href="https://github.com/JMRosenberg12/Note-Taker" role="button">Github Repoistory to Note Taker</a>
                 </div>
                 <div className="col-sm">
-                  <img src="./assets/images/Project2.png" alt="Project 2"></img>
+                  <img src="./images/Project2.png" alt="Project 2"></img>
                   <a className="btn btn-primary" href="https://spiderfly.herokuapp.com" role="button">Project 2</a>
                   <a className="btn btn-success" href="https://github.com/icek567/Project-2" role="button">Github Repository to Project 2</a>
                 </div>
